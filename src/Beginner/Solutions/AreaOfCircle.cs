f68341273c703873430930b76438e0b14066f644
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2021-02-18 22:37:17 -0300

    add AreaOfCircle

---
+++ src/Beginner/Solutions/AreaOfCircle.cs
@@ -0,0 +1,33 @@
+﻿/*
+   * URI Online Judge | 1002
+   * The formula to calculate the area of a circumference is defined as A = π . R2. Considering to this problem that π = 3.14159:
+   * Calculate the area using the formula given in the problem description.
+*/
+using System;
+
+namespace Uri.Projects.Beginner.Solutions
+{
+    public class AreaOfCircle
+    {
+        private const double PI = 3.14159;
+        public string GetSolutionResponse()
+        {
+            var radius = ReadAndParseDoubleValue();
+            var solutionStringFormat = GetSolutionStringFormat();
+            var solutionResponseValue = CircumferenceCalculate(radius);
+
+            return string.Format(solutionStringFormat, solutionResponseValue);
+        }
+
+        public double ReadAndParseDoubleValue()
+        {
+            var value = Console.ReadLine();
+            return double.Parse(value);
+        }
+
+        public string GetSolutionStringFormat() => "A={0}";
+
+        public double CircumferenceCalculate(double radius) => Math.Round(PI * Math.Pow(radius, 2), 4);
+
+    }
+}
---
+++ test/Beginner.Test/Solutions/AreaOfCircleTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Uri.Projects.Beginner.Solutions;
+using Xunit;
+
+namespace Beginner.Test.Solutions
+{
+    public class AreaOfCircleTest
+    {
+        [Theory]
+        [InlineData("2,00", "A=12.5664")]
+        [InlineData("100,64", "A=31819.3103")]
+        [InlineData("150,00", "A=70685.7750")]
+        public void AreaOfCircleMustBeReturnSucess(string radius, string expectedResponse)
+        {
+            // Arrange
+            var solution = new AreaOfCircle();
+
+            var inputText = new StringBuilder(radius);
+
+            var output = new StringWriter();
+            Console.SetOut(output);
+
+            var input = new StringReader(inputText.ToString());
+            Console.SetIn(input);
+
+            // Act
+            var solutionResponse = solution.GetSolutionResponse();
+
+            // Assert
+            Assert.Equal(expectedResponse, solutionResponse);
+        }
+    }
+}
